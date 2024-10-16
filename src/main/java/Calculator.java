849fe214236c72d3691aa3fb7263291d621d3c3c
mwdziak <mwdziak@gmail.com>
2024-10-16 10:16:52 +0200

    Division implemented

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,4 +8,7 @@ public class Calculator {
     public static int Multiply(int a, int b) {
         return a * b;
     }
+    public static double Divide(int a, int b) {
+        return (double) a / b;
+    }
 }
